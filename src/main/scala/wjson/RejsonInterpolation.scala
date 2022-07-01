dd696209c188e9f0a954d45fc05fa2e733366067
WahsonLeung <hsliang1@163.com>
2022-07-01 18:20:47 +0800

    add implement for rejson's pattern matching

--- src/main/scala/wjson/RejsonInterpolation.scala
+++ src/main/scala/wjson/RejsonInterpolation.scala
@@ -1,8 +1,176 @@
 package wjson
 
+import wjson.*
+import wjson.JsPattern.Variable
+import wjson.JsPattern._
+
+import scala.jdk.CollectionConverters.*
+
 class RejsonInterpolation(sc: StringContext) {
-
-  def unapplySeq(input: JsValue): Option[Seq[Any]] =
-    ???
-
+  
+  object Placeholder {
+    
+    def apply(index: Int): JsString = JsString("placeholder_" + index + "_")
+    
+    def unapply(str: String): Option[Int] = {
+      str match
+        case str if str.startsWith("placeholder_") && str.endsWith("_") =>
+          Some(str.substring(12, str.length - 1).toInt)
+        case _ => None
+    }
+  }
+  
+  object InlineKey {
+    def unapply(str: String): Option[Seq[String]] = {
+      str match
+        case str if str.contains("/") =>
+          Some(str.split("/").toSeq.filter(_.nonEmpty))
+        case _ => None
+    }
+  }
+  
+  def unapplyAsMap(input: JsValue): Option[Map[String, Any]] = ???
+  
+  def unapplySeq(input: JsValue): Option[Seq[Any]] = {
+    
+    val str = if sc.parts.size > 1 then
+      sc.parts.head + sc.parts.tail.zipWithIndex.map { case (p, idx) => Placeholder(idx).value + p }.mkString
+    else sc.parts.head
+    
+    val pattern = JsPatternParser.parseRejson(str)
+    println(pattern)
+    val results = collection.mutable.ArrayBuffer[Any]()
+    Seq.range(0, sc.parts.length - 1).foreach { x => results += null }
+    
+    try {
+      patternMatch(pattern, input, results)
+      Some(results.toSeq)
+    }
+    catch {
+      case ex: Throwable =>
+        None
+    }
+  }
+  
+  private def parseType(value: JsValue, `type`: JsPattern) = `type` match
+    case JsPattern.AnyVal(GroundType.STRING) => value.asInstanceOf[JsString].value
+    case JsPattern.AnyVal(GroundType.BOOLEAN) => value.asInstanceOf[JsBoolean].value
+    case JsPattern.AnyVal(GroundType.INTEGER) =>
+      assert(value.asInstanceOf[JsNumber].value.isValidInt)
+      value.asInstanceOf[JsNumber].value.toInt
+    case JsPattern.AnyVal(GroundType.NUMBER) => value.asInstanceOf[JsNumber].value
+    case JsPattern.StringPattern(_value) =>
+      assert(value.isInstanceOf[JsString] && value.asInstanceOf[JsString].value == _value)
+      _value
+    case JsPattern.NumberPattern(_value) =>
+      assert(value.isInstanceOf[JsNumber] && value.asInstanceOf[JsNumber].value == _value)
+      _value
+    case JsPattern.BoolPattern(_value) =>
+      assert(value.isInstanceOf[JsBoolean] && value.asInstanceOf[JsBoolean].value == _value)
+      _value
+    case _ => value
+  
+  private def exactInlineKeys(unmerge: Map[String, Variable]): Map[String, Variable] = {
+    unmerge
+  }
+  
+  private def objPatternMatch(value: Map[String, Variable], input: JsValue,
+                              results: collection.mutable.ArrayBuffer[Any]): Unit = {
+    assert(input.isInstanceOf[JsObject])
+    val inputObj = input.asInstanceOf[JsObject]
+    
+    val (_declared, others) = value.partition(_._2.pattern != AnyVals())
+  
+    val declared = exactInlineKeys(_declared)
+  
+    others.values.headOption match
+      case Some(Variable(p, _)) =>
+        val declaredKeys = declared.keys.toSet
+        assert(declaredKeys -- inputObj.fields.keys.toSet == Set.empty)
+        p match
+          case Placeholder(index) =>
+            results(index) = inputObj.fields.filterNot { case (k, _) => declaredKeys.contains(k) }.toMap
+          case _ =>
+      case _ =>
+        assert(declared.keys.toSet == inputObj.fields.keys.toSet)
+    
+    declared.foreach {
+      case (key, Variable(Placeholder(index), ptn: ArrPattern)) =>
+        assert(inputObj.fields contains key)
+        results(index) = parseType(inputObj.fields(key), ptn)
+        patternMatch(Variable(null, ptn), inputObj.fields(key), results)
+      
+      case (key, Variable(Placeholder(index), ptn: ObjPattern)) =>
+        assert(inputObj.fields contains key)
+        results(index) = parseType(inputObj.fields(key), ptn)
+        patternMatch(Variable(null, ptn), inputObj.fields(key), results)
+      
+      case (key, Variable(Placeholder(index), ptn)) =>
+        assert(inputObj.fields contains key)
+        results(index) = parseType(inputObj.fields(key), ptn)
+      
+      case (key, _value) =>
+        assert(inputObj.fields contains key)
+        patternMatch(_value, inputObj.fields(key), results)
+    }
+  }
+  
+  private def arrPatternMatch(value: List[Variable], input: JsValue,
+                              results: collection.mutable.ArrayBuffer[Any]): Unit = {
+    assert(input.isInstanceOf[JsArray])
+    val inputArr = input.asInstanceOf[JsArray]
+    
+    if (value.exists(_.pattern == JsPattern.AnyVals())) {
+      assert(input.asInstanceOf[JsArray].elements.size >= value.size - 1)
+    } else {
+      assert(input.asInstanceOf[JsArray].elements.size == value.size)
+    }
+    value.zipWithIndex.foreach {
+      case (Variable(Placeholder(index), JsPattern.AnyVals()), idx) =>
+        results(index) = inputArr.elements.takeRight(inputArr.elements.size - idx)
+      case (Variable(Placeholder(index), ptn), y: Int) =>
+        results(index) = parseType(inputArr.elements(y), ptn)
+      case (x: Variable, y: Int) =>
+        patternMatch(x, inputArr.elements.apply(y), results)
+    }
+  }
+  
+  private def patternMatch(pattern: Variable, input: JsValue,
+                           results: collection.mutable.ArrayBuffer[Any]): Unit = pattern match
+    
+    case Variable(_, JsPattern.ObjPattern(value)) =>
+      objPatternMatch(value, input, results)
+    
+    case Variable(_, JsPattern.ArrPattern(value)) =>
+      arrPatternMatch(value, input, results)
+    
+    case Variable(_, JsPattern.AnyVal(GroundType.STRING)) =>
+      assert(input.isInstanceOf[JsString])
+    
+    case Variable(_, JsPattern.AnyVal(GroundType.INTEGER)) =>
+      assert(input.isInstanceOf[JsNumber] && input.asInstanceOf[JsNumber].value.isValidInt)
+    
+    case Variable(_, JsPattern.AnyVal(GroundType.NUMBER)) =>
+      assert(input.isInstanceOf[JsNumber])
+    
+    case Variable(_, JsPattern.AnyVal(GroundType.BOOLEAN)) =>
+      assert(input.isInstanceOf[JsBoolean])
+    
+    case Variable(_, JsPattern.AnyVal(GroundType.ANY)) =>
+      assert(true)
+    
+    case Variable(_, JsPattern.StringPattern(value)) =>
+      assert(input.isInstanceOf[JsString] && input.asInstanceOf[JsString].value == value)
+    
+    case Variable(_, JsPattern.NumberPattern(value)) =>
+      assert(input.isInstanceOf[JsNumber] && input.asInstanceOf[JsNumber].value == value)
+    
+    case Variable(_, JsPattern.BoolPattern(value)) =>
+      assert(input.isInstanceOf[JsBoolean] && input.asInstanceOf[JsBoolean].value == value)
+    
+    case Variable(_, JsPattern.TaggedString(tag, content)) =>
+      // todo
+      assert(true)
+    case Variable(_, JsPattern.AnyVals()) =>
+  
 }
--- src/test/scala/wjson_test/TestPatterns.scala
+++ src/test/scala/wjson_test/TestPatterns.scala
@@ -1,26 +1,240 @@
 package wjson_test
 
 import org.scalatest.funsuite.AnyFunSuite
-import wjson.*
+import wjson.JsValue.{JsArray, JsNumber}
+import wjson.{given, *}
 
 class TestPatterns extends AnyFunSuite {
-
+  
   test("simple patterns") {
-    val js = json"""{
+    val js =
+      json"""{
       "a": 1,
-      "b": "hello",
-      "c": true
+      "b": "123",
+      "c": true,
+      "d": "ddd"
     }"""
-
+    
     js match {
       case rejson"""{
         "a": ${a}@integer,
         "b": ${b}@string,
-        "c": ${c}@boolean
-      }""" =>
-        println(s"a=$a, b=$b, c=$c")
+        "c": ${c}@_,
+        "d": ${d}@"ddd"
+        }""" =>
+        println(s"a=$a, b=$b, c=$c, d=$d")
+        assert(a == 1)
+        assert(b == "123")
+        assert(c.asInstanceOf[JsBoolean].value == true)
+        assert(d == "ddd")
     }
-
+    
   }
-
+  
+  
+  test("test type matching") {
+    val js =
+      json"""{
+      "a": 1,
+      "b": "hello",
+      "c": true,
+      "d": 12.3
+    }"""
+    
+    js match {
+      case rejson"""
+         {
+          "a": integer,
+          "b": string,
+          "c": boolean,
+          "d": number
+        }""" =>
+      case _ => assert(false)
+    }
+    
+    js match {
+      case rejson"""
+         {
+          "a": ${a}@integer,
+          "b": ${b}@string,
+          "c": ${c}@boolean,
+          "d": ${d}@number
+        }""" =>
+        assert(a == 1)
+        assert(b == "hello")
+        assert(c == true)
+        assert(d == 12.3)
+      case _ => assert(true)
+    }
+    
+  }
+  
+  test("test value matching") {
+    val js =
+      json"""{
+      "a": 1,
+      "b": "hello",
+      "c": true,
+      "d": 12.3,
+      "e": -12
+    }"""
+    
+    js match {
+      case rejson"""
+         {
+          "a": 1,
+          "b": "hello",
+          "c": true,
+          "d": 12.3,
+          "e": -12
+        }""" => assert(true)
+      case _ => assert(false)
+    }
+    
+    js match {
+      case rejson"""
+         {
+          "a": ${a}@1,
+          "b": ${b}@"hello",
+          "c": ${c}@true,
+          "d": ${d}@12.3,
+          "e": ${e}@-12
+        }""" =>
+        assert(a == 1)
+        assert(b == "hello")
+        assert(c == true)
+        assert(d == 12.3)
+        assert(e == -12)
+      case _ => assert(false)
+    }
+    
+  }
+  
+  test("test array matching") {
+    val js =
+      json"""{
+      "a": [1,2,3],
+    }"""
+    
+    js match {
+      case rejson"""
+         {
+          "a": $arr@[1,2,3],
+        }""" =>
+        assert(arr == JsArray(JsNumber(1), JsNumber(2), JsNumber(3)))
+      case _ =>
+        assert(false, "should not match")
+    }
+  
+    js match {
+      case rejson"""
+         {
+          "a": $arr@[1,2,4],
+        }""" =>
+        assert(arr == JsArray(JsNumber(1), JsNumber(2), JsNumber(3)))
+      case _ =>
+        assert(true, "should not match")
+    }
+    
+    js match {
+      case rejson"""
+         {
+          "a": ${arr}@[${a1}@1,${a2}@integer,3],
+        }""" =>
+        assert(arr == JsArray(JsNumber(1), JsNumber(2), JsNumber(3)))
+        assert(a1 == 1)
+        assert(a2 == 2)
+      case _ => assert(false)
+    }
+    js match {
+      case rejson"""
+         {
+          "a": ${arr}@_,
+        }""" =>
+        assert(arr.asInstanceOf[JsArray].elements.size == 3)
+      case _ => assert(false)
+    }
+  }
+  
+  test("test nesting object matching") {
+    val js =
+      json"""{
+      "a": {"b": {c: "ccc", d: 123}}
+    }"""
+    
+    js match {
+      case rejson"""
+         {
+          a: ${a}@{
+            b: {
+              c: ${c}@"ccc",
+              d: ${d}@123,
+            }
+          }
+        }""" =>
+        assert(a == JsObject("b" -> JsObject("c" -> JsString("ccc"), "d" -> JsNumber(123))))
+        assert(c == "ccc")
+        assert(d == 123)
+      case _ => assert(false)
+    }
+    
+//    js match {
+//      case rejson"""
+//         {
+//          a/b/c: ${c}@"ccc",
+//          a/b/d: ${d}@123,
+//        }""" =>
+//        assert(c == "ccc")
+//        assert(d == 123)
+//      case _ => assert(false)
+//    }
+  }
+  
+  test("test anyVals in object") {
+    val js =
+      json"""{
+      "a": "1",
+      "b": 2,
+      "c": true,
+    }"""
+    
+    js match {
+      case rejson"""
+         {
+          a: ${a}@"1",
+          ${other}@_*,
+          b: ${b}@2,
+        }""" =>
+        assert(a == "1")
+        assert(b == 2)
+        assert(other == Map("c" -> JsBoolean(true)))
+      case _ => assert(false)
+    }
+  }
+  
+  test("test anyVals in array") {
+    val js =
+      json"""{
+      "a": [1,2,3]
+    }"""
+    
+    js match {
+      case rejson"""
+         {
+          a: ${a}@[1,2,3, ${other}@_*],
+        }""" => 
+        assert(a == JsArray(JsNumber(1), JsNumber(2), JsNumber(3)))
+        assert(other == List())
+      case _ => assert(false)
+    }
+    js match {
+      case rejson"""
+         {
+          a: ${a}@[1,2,${other}@_*],
+        }""" =>
+        assert(a == JsArray(JsNumber(1), JsNumber(2), JsNumber(3)))
+        assert(other == List(JsNumber(3)))
+      case _ => assert(false)
+    }
+  }
 }
